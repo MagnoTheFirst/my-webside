e2668803a9593379c7183a002a6f4adcc97a01f6
MagnoTheFirst <alejandro.r.laneri@gmail.com>
2021-12-12 13:16:18 +0100

    first lady

---
+++ src/main/java/ch/webside/mywebside/MyWebSideApplication.java
@@ -0,0 +1,13 @@
+package ch.webside.mywebside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebSideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWebSideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/webside/mywebside/MyWebSideApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.webside.mywebside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebSideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
